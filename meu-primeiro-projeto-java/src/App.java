40637a3873b89963498a52694c33da889952886a
thikogit <thiago.silva391@fatec.sp.gov.br>
2024-05-25 13:55:41 -0300

    Alterações e novos arquivos

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Salve, salve Turma DIO!");
+        System.out.println("Bora aprender Java básico com o Glayson");
+    }
+}
